96edb29af1e085817ec7845bddb205124c23d1c7
atrox3d <atrox3d@gmail.com>
2024-09-28 10:50:34 +0200

    add separate routes

--- app/routes.py
+++ app/routes.py
@@ -5,14 +5,15 @@ from app.models import Todo
 
 main = Blueprint("main", __name__)
 
-@main.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        db.session.add(Todo(task=request.form["task"]))
-        db.session.commit()
-        
-        return redirect(url_for("main.index"))
-    elif request.method == "GET":
-        todos = Todo.query.all()
+# @main.route("/", methods=["GET", "POST"])
+@main.post("/")
+def create_todo():
+    db.session.add(Todo(task=request.form["task"]))
+    db.session.commit()
+    
+    return redirect(url_for("main.get_todos"))
 
+@main.get("/")
+def get_todos():
+        todos = Todo.query.all()
         return render_template("index.html", todos=todos)
