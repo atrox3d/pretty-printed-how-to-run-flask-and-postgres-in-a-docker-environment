8453cf91789e585e96fd1a4ae235c78dd20c5ce8
atrox3d <atrox3d@gmail.com>
2024-09-30 00:52:10 +0200

    try standard logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,26 @@
 from flask import Flask 
+import logging
 
 from app.extensions import db
 from app.routes import main
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+def print_app_env(app:Flask):
+    logger.info(f'FLASK_SQLALCHEMY_DATABASE_URI = {app.config.get('FLASK_SQLALCHEMY_DATABASE_URI')}')
+    logger.info(f'SQLALCHEMY_DATABASE_URI = {app.config.get('SQLALCHEMY_DATABASE_URI')}')
+
+    logger.info(f'FLASK_PYTHONUNBUFFERD = {app.config.get('FLASK_PYTHONUNBUFFERD')}')
+    logger.info(f'PYTHONUNBUFFERD = {app.config.get('PYTHONUNBUFFERD')}')
+
+
 def create_app():
+    logger.info('create_app')
+
     app = Flask(__name__)
     app.config.from_prefixed_env()
+    print_app_env(app)
 
     db.init_app(app)
     with app.app_context():
@@ -14,4 +29,3 @@ def create_app():
     app.register_blueprint(main)
 
     return app
-    
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+import logging
 
 from app.extensions import db
 from app.models import Todo
 
+logger = logging.getLogger(__name__)
 main = Blueprint("main", __name__)
 
 # @main.route("/", methods=["GET", "POST"])
 @main.post("/")
 def create_todo():
+    logger.info('create--------------------')
     db.session.add(Todo(task=request.form["task"]))
     db.session.commit()
     
@@ -15,5 +18,6 @@ def create_todo():
 
 @main.get("/")
 def get_todos():
-        todos = Todo.query.all()
-        return render_template("index.html", todos=todos)
+    logger.info('get--------------------')
+    todos = Todo.query.all()
+    return render_template("index.html", todos=todos)
